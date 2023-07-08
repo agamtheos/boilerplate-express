2e6bc26e92464a2a258451acab18ee9840a6a0aa
Agam Theos <agamtheos@gmail.com>
2023-07-08 21:12:08 +0700

    build: initialize boilerplate

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const { json, urlencoded } = require('body-parser');
+
+const env = require('./config/env');
+const response = require('./helpers/response');
+
+const app = express();
+
+app.use(cors({credentials: true, origin: true}));
+app.use(json());
+app.use(response);
+
+app.listen(env.port, function() {
+    console.log('Server is running on port ', env.port);
+})
