288e91d60dbc3ff7d0e7c891320c735b03a02f55
Agam Theos <agamtheos@gmail.com>
2023-07-08 21:19:19 +0700

    feat: add routes dir

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { json, urlencoded } = require('body-parser');
 
+const Router = require('./routes/index.router');
 const env = require('./config/env');
 const response = require('./helpers/response');
 
@@ -10,6 +11,7 @@ const app = express();
 app.use(cors({credentials: true, origin: true}));
 app.use(json());
 app.use(response);
+app.use('/api', Router)
 
 app.listen(env.port, function() {
     console.log('Server is running on port ', env.port);
